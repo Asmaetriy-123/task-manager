197659aee467e9d2375d7847b8cd0a1be9b11a2a
Asmae Triy <asmaetriy123@gmail.com>
2025-04-24 04:58:48 +0100

    merging the navbar

---
+++ public/navbar.js
@@ -0,0 +1,30 @@
+const date_element=document.getElementById("date")
+const today=new Date()
+const optionsWeekDay={weekday:'long'};
+const optionsDay={day:'numeric'};
+const optionsMonth={month:'long'};
+const optionsYear={year:'numeric'};
+const formattedDate=today.toLocaleDateString(undefined,optionsWeekDay)+" " +
+today.toLocaleDateString(undefined,optionsDay)+" "+
+today.toLocaleDateString(undefined,optionsMonth)+  " , "+
+today.toLocaleDateString(undefined,optionsYear)
+date_element.textContent=formattedDate;
+console.log(formattedDate)
+//showing the search bar when the search icon is clicked
+
+
+const search_icon=document.getElementById("search_icon");
+const search_bar=document.getElementById('input_search_bar');
+
+search_icon.addEventListener('click',function(){
+
+    search_bar.classList.toggle("visible");
+})
+
+//adding a drop_down menu when hitting th profile icon
+const profileIcon = document.getElementById("profile_icon");
+const profileWrapper = document.querySelector(".profile_wrapper");
+
+profileIcon.addEventListener("click", () => {
+  profileWrapper.classList.toggle("show");
+});
